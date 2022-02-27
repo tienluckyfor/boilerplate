963e8bd8c071414dea0df4dcb01f4a82c83c53a9
tien.luckyfor <tien.luckyfor@gmail.com>
2022-02-27 23:06:50 +0700

    loginPage

--- ts-react-tailwindui/src/pages/auth/LoginPage.tsx
+++ ts-react-tailwindui/src/pages/auth/LoginPage.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
-import useRegister from "./useRegister";
 import AuthLayout from "layouts/AuthLayout";
 import TextField from "components/Shared/Fields/Text";
+import useLogin from "./useLogin";
 
 const RegisterPage: React.FC<{}> = () => {
-    const formik = useRegister()
+    const formik = useLogin()
     return (
-        <AuthLayout title={"Register"}>
+        <AuthLayout title={"Login"}>
             <form
                 onSubmit={formik.handleSubmit}
                 className="space-y-6">
@@ -17,23 +17,6 @@ const RegisterPage: React.FC<{}> = () => {
                     type={"email"}
                     formik={formik}
                 />
-                <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                        Email address
-                    </label>
-                    <div className="mt-1">
-                        <input
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                    </div>
-                    <p className="text-red-500 capitalize-first line-clamp-2">{formik.errors.email}</p>
-                </div>
                 <div>
                     <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                         Password
@@ -51,22 +34,12 @@ const RegisterPage: React.FC<{}> = () => {
                     </div>
                     <p className="text-red-500 capitalize-first">{formik.errors.password}</p>
                 </div>
-                <div>
-                    <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-                        Confirm Password
-                    </label>
-                    <div className="mt-1">
-                        <input
-                            onChange={formik.handleChange}
-                            value={formik.values.confirmPassword}
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            type="password"
-                            autoComplete="current-password"
-                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
+                <div className="flex items-center justify-end">
+                    <div className="text-sm">
+                        <Link to="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            Forgot your password?
+                        </Link>
                     </div>
-                    <p className="text-red-500 capitalize-first">{formik.errors.confirmPassword}</p>
                 </div>
                 <div>
                     <button
@@ -80,8 +53,8 @@ const RegisterPage: React.FC<{}> = () => {
 
             <div className="flex items-center justify-center mt-6">
                 <div className="">
-                    <Link to="/LoginPage" className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Already have an account? Login
+                    <Link to="/RegisterPage" className="font-medium text-indigo-600 hover:text-indigo-500">
+                        Register
                     </Link>
                 </div>
             </div>
